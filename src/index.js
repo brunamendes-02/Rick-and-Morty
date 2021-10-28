5fe7e036a8c4196f7bafc5429869efdca161d526
Bruna Mendes <bruna.souza@kbase.com.br>
2021-10-28 07:15:27 -0300

    removendo comentarios

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 import App from './App'
 
-// nova instancia do apolo client
 const client = new ApolloClient({
   uri: 'https://rickandmortyapi.com/graphql',
   cache: new InMemoryCache()
